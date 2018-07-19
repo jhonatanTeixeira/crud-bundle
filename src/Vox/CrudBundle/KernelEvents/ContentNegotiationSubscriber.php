2037cfe4ec73c70dc4c20541c1e37d6938084e34
Jhonatan Teixeira <jhonatan.teixeira@gmail.com>
2018-07-19 15:33:11 -0300

    fixes content

--- src/Vox/CrudBundle/KernelEvents/ContentNegotiationSubscriber.php
+++ src/Vox/CrudBundle/KernelEvents/ContentNegotiationSubscriber.php
@@ -15,19 +15,19 @@ class ContentNegotiationSubscriber implements EventSubscriberInterface
      * @var SerializerInterface
      */
     private $serializer;
-    
+
     /**
      * @var Response
      */
     private $response;
-    
+
     private $template;
-    
+
     /**
      * @var \Twig_Environment
      */
     private $twig;
-    
+
     public function __construct(SerializerInterface $serializer, \Twig_Environment $twig)
     {
         $this->serializer = $serializer;
@@ -50,34 +50,35 @@ class ContentNegotiationSubscriber implements EventSubscriberInterface
             ]
         ];
     }
-    
+
     public function setTemplate(\Symfony\Component\HttpKernel\Event\FilterControllerEvent $event)
     {
         list($controller, $action) = $event->getController();
-        
+
         if (!$controller instanceof \Vox\CrudBundle\Controller\CrudController) {
             return;
         }
-        
+
         $templates = $controller->getTemplates();
-        
+
         if ($action == 'post' || $action == 'put') {
             $action = 'form';
         }
-        
+
         $actions = [
             'post' => 'form',
             'put'  => 'form',
             'get'  => 'form',
             'form'  => 'form',
+            'receiveData'  => 'form',
             'list' => 'list'
         ];
-        
+
         $action = $actions[str_replace('Action', '', $action)];
-        
+
         $this->template = $templates["{$action}Template"] ?? null;
     }
-    
+
     public function renderView(GetResponseForControllerResultEvent $event)
     {
         if (!$event->getRequest()->getRequestFormat() == 'html' || !$this->template) {
@@ -89,10 +90,10 @@ class ContentNegotiationSubscriber implements EventSubscriberInterface
         $status = $result['status'] ?? 200;
 
         $data = $this->twig->render($this->template, $event->getControllerResult());
-        
+
         $event->setResponse(new Response($data, $status));
     }
-    
+
     public function serialize(GetResponseForControllerResultEvent $event)
     {
         if (in_array($event->getRequest()->getRequestFormat(), ['json', 'xml'])) {
@@ -101,19 +102,19 @@ class ContentNegotiationSubscriber implements EventSubscriberInterface
             if (!is_array($result) && !is_object($result)) {
                 return;
             }
-            
+
             $this->response = new Response(
                 $this->serializer->serialize($result, $event->getRequest()->getRequestFormat())
             );
         }
     }
-    
+
     public function setResponse(GetResponseForControllerResultEvent $event)
     {
         if ($this->response) {
             $event->setResponse($this->response);
         }
-        
+
         $this->response = null;
     }
 
@@ -123,7 +124,7 @@ class ContentNegotiationSubscriber implements EventSubscriberInterface
 
         if (in_array($request->getRequestFormat(), ['json']) && $request->getMethod() == 'POST') {
             $content = json_decode($request->getContent(), true);
-            
+
             $request->request->add($content);
         }
     }
